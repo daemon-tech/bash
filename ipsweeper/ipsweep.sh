0c4d9502c8e2033e16d042a106951cd9b3b11d06
le3ch-tech <sam.moes15@gmail.com>
2020-08-25 06:06:22 +0200

    Ipsweeper script

---
+++ ipsweeper/ipsweep.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+if [ "$1" == "" ]
+then
+echo "IP address missing!"
+echo "Syntax: ./ipsweep.sh iphere"
+
+else
+for ip in `seq 1 254`; do
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+done
+fi
+
